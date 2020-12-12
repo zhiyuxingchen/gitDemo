df7268da3892dccae5dd23bdb6d1e3ebaa878fd7
mao <605590883@qq.com>
2020-12-12 20:20:08 +0800

    aa

---
+++ src/A.java
@@ -0,0 +1,10 @@
+public class A {
+    public static void main(String[] args) {
+
+        System.out.println("...11111..");
+
+        System.out.println("qqqqqqqq");
+        System.out.println(".ignore");
+        System.out.println("main");
+    }
+}
